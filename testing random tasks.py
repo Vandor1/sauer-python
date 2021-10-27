eafff81ab3ed993c7eb8aabf77f8922eeb744c25
Mathias <mvbend@stud.ntnu.no>
2021-10-27 18:45:38 +0200

    4.2 2b!

--- testing random tasks.py
+++ testing random tasks.py
@@ -1,40 +1,31 @@
 import numpy as np
-from scipy.linalg import lu
-import math
+import matplotlib.pyplot as plt
 
 
 # conditional values etc.
 def main():
-    #4.1 2a)
-    A = np.array([[1,1,0],[0,1,1],[1,2,1],[1,0,1]])
-    b = np.array([2,2,3,4]).reshape(4,1)
-    x = np.array([2,-1/3,2]).reshape(3,1)
-    z = b- np.dot(A,x)
-    print(np.sqrt((z[0]**2 + z[1]**2 + z[2]**2 + z[3]**2)/2))
+    # 4.1 2a)
+    A = np.array([[1, 1, 0], [0, 1, 1], [1, 2, 1], [1, 0, 1]])
+    b = np.array([2, 2, 3, 4]).reshape(4, 1)
+    x = np.array([2, -1 / 3, 2]).reshape(3, 1)
+    z = b - np.dot(A, x)
+    print(np.sqrt((z[0] ** 2 + z[1] ** 2 + z[2] ** 2 + z[3] ** 2) / 2))
 
-    #4.1 3)
-    c = [[1,0],[1,0],[1,0]]
-    d = [[1],[5],[6]]
+    # 4.1 3)
+    c = [[1, 0], [1, 0], [1, 0]]
+    d = [[1], [5], [6]]
     ct = np.transpose(c)
-    print(np.dot(ct,c))
+    print(np.dot(ct, c))
 
-    #4.1 8a)
-    e = [[1,1,1,1],[0,1,2,5]]
-    g = [[1,0],[1,1],[1,2],[1,5]]
-    b2 = [[0],[3],[3],[6]]
-    print(np.dot(e,g))
-    print(np.linalg.lstsq(g,b2,rcond=None))
-    x2 = [[0.857142],[15/14]]
-    z2 = b2-np.dot(g,x2)
-    print(np.sqrt((z2[0]**2 + z2[1]**2 + z2[2]**2 + z2[3]**2)/2))
-
-    # A = np.matrix([[1, 2.01], [3, 6]])
-    # B = np.matrix([[1, 2], [3, 4]])
-    # eigB = np.linalg.eig(B)
-    # Ainf = np.linalg.norm(A, np.inf)
-    # Acond = np.linalg.cond(A)
-    # inverse = np.linalg.inv(A)
-    # print(eigB)
+    # 4.1 8a)
+    e = [[1, 1, 1, 1], [0, 1, 2, 5]]
+    g = [[1, 0], [1, 1], [1, 2], [1, 5]]
+    b2 = [[0], [3], [3], [6]]
+    print(np.dot(e, g))
+    print(np.linalg.lstsq(g, b2, rcond=None))
+    x2 = [[0.857142], [15 / 14]]
+    z2 = b2 - np.dot(g, x2)
+    print(np.sqrt((z2[0] ** 2 + z2[1] ** 2 + z2[2] ** 2 + z2[3] ** 2) / 2))
 
 
 # finding norms.
@@ -47,7 +38,7 @@ def main2():
 # PA = LU Factorization using scipy.
 def main3():
     # 2.4, 2c) A = np.array([[1, 2, -3], [2, 4, 2], [-1, 0, 3]])
-    #2.4, 3b)
+    # 2.4, 3b)
     # A = np.array([[3, 1, 2], [6, 3, 4], [3, 1, 5]])
     # # A = np.array([[-1, 0, 1], [2, 1, 1], [-1, 2, 0]])
     # B = np.array([[1, 2, -1, 2], [0, 3, 1, 4], [2, -1, 1, 2]])
@@ -58,25 +49,56 @@ def main3():
     # print(p)
     # print(l)
     # print(u)
-    Q = np.array([[2/3,-1/3],[-2/3,-2/3],[1/3,-2/3]])
-    R = np.array([[3,6],[0,3]])
-    print(np.dot(Q,R))
+    Q = np.array([[2 / 3, -1 / 3], [-2 / 3, -2 / 3], [1 / 3, -2 / 3]])
+    R = np.array([[3, 6], [0, 3]])
+    print(np.dot(Q, R))
 
 
-import sympy as sy
+def main4():
+    # F3(t)
+    A = np.array([
+        [1.0, np.cos(0.0), np.sin(0.0)],
+        [1.0, np.cos(np.pi / 3.0), np.sin(np.pi / 3.0)],
+        [1.0, np.cos(np.pi * (2.0 / 3.0)), np.sin(np.pi * (2.0 / 3.0))],
+        [1.0, np.cos(np.pi), np.sin(np.pi)],
+        [1.0, np.cos(np.pi * (4.0 / 3.0)), np.sin(np.pi * (4.0 / 3.0))],
+        [1.0, np.cos(np.pi * (10.0 / 6.0)), np.sin(np.pi * (10.0 / 6.0))]
+    ])
+    b = np.array([4, 2, 0, -5, -1, 3]).reshape((6, 1))
+    b2 = np.array([4, 2, 0, -5, -1, 3])
+    leastSquares = np.linalg.lstsq(A, b, rcond=None)[0]
+    f3 = lambda t: leastSquares[0] + np.cos(2 * np.pi * t) * leastSquares[1] + np.sin(2 * np.pi * t) * leastSquares[2]
+    i = np.array([0, 1 / 6, 1 / 3, 1 / 2, 2 / 3, 5 / 6], dtype=np.int64)
+    RMSE1 = np.sqrt(sum((b2 - f3(i)) ** 2) / len(i))
+    print(RMSE1)
+    # x_range = np.linspace(0, 3, 1000)
+    # plt.scatter(i, b2)
+    # plt.plot(x_range, f3(x_range), c='g')
+    # plt.show()
 
-def f(x):
-    return x ** 4 * (sy.sin(x) * sy.cos(x) +1)
+    # F4(t)
+    A2 = np.array([
+        [1.0, np.cos(0.0), np.sin(0.0), np.cos(0 * 4.0 * np.pi)],
+        [1.0, np.cos(np.pi / 3.0), np.sin(np.pi / 3.0), np.cos(4.0 * np.pi * (1 / 6))],
+        [1.0, np.cos(np.pi * (2.0 / 3.0)), np.sin(np.pi * (2.0 / 3.0)), np.cos(4.0 * np.pi * (1 / 3))],
+        [1.0, np.cos(np.pi), np.sin(np.pi), np.cos(4.0 * np.pi * (1 / 2))],
+        [1.0, np.cos(np.pi * (4.0 / 3.0)), np.sin(np.pi * (4.0 / 3.0)), np.cos(4.0 * np.pi * (2 / 3))],
+        [1.0, np.cos(np.pi * (10.0 / 6.0)), np.sin(np.pi * (10.0 / 6.0)), np.cos(4.0 * np.pi * (5 / 6))]
+    ])
+    b3 = np.array([4, 2, 0, -5, -1, 3]).reshape((6, 1))
+    b4 = np.array([4, 2, 0, -5, -1, 3])
+    leastSquares2 = np.linalg.lstsq(A2, b3, rcond=None)[0]
+    f4 = lambda t: leastSquares2[0] + np.cos(2 * np.pi * t) * leastSquares2[1] + np.sin(2 * np.pi * t) * leastSquares2[
+        2] + leastSquares2[3] * np.cos(4 * np.pi * t)
+    RMSE2 = np.sqrt(sum((b4 - f4(i)) ** 2) / len(i))
+    print(RMSE2)
 
-import math
+    print("    ")
+    if (RMSE2 < RMSE1):
+        print("Second model is best!")
+    else:
+        print("First model is best!")
 
-def g(x):
-    return 3*math.exp(1-x**2)
 
 if __name__ == "__main__":
-    main()
-    # print(g(1.1))
-    #
-    # x = sy.Symbol("x")
-    # print(sy.integrate(f(x), (x, -3.0, 3.0)))
-
+    main4()
